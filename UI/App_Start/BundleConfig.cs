0c90ed4ffbd149b25bb54e7c8f057e6a48d8b6be
dabakura <38744169+dabakura@users.noreply.github.com>
2019-04-09 16:18:03 -0600

    Plantilla de Inicio

--- UI/App_Start/BundleConfig.cs
+++ UI/App_Start/BundleConfig.cs
@@ -25,6 +25,28 @@ namespace UI
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/greensock").Include("~/Scripts/TweenMax.min.js","~/Scripts/TimelineMax.min.js","~/Scripts/animation.gsap.min.js","~/Scripts/ScrollToPlugin.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/scrollmagic").Include(
+                      "~/Scripts/ScrollMagic.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/OwlCarousel2").Include(
+                      "~/Scripts/owl.carousel.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/slick").Include(
+                      "~/Scripts/slick.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/easing").Include(
+                      "~/Scripts/easing.js"));
+
+            bundles.Add(new StyleBundle("~/Content/OwlCarousel2css").Include(
+                      "~/Content/owl.carousel.css",
+                      "~/Content/owl.theme.default.css",
+                      "~/Content/animate.css"));
+
+            bundles.Add(new StyleBundle("~/Content/slickcss").Include(
+                      "~/Content/slick.css"));
         }
     }
 }
